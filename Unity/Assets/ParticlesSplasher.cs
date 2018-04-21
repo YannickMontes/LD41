e2ae651c3b56e54dff393a5009d5b3fd730d679a
YannickMontes <montes.yannick@gmail.com>
2018-04-21 20:35:07 +0200

    Particle splasher change color

---
+++ Unity/Assets/ParticlesSplasher.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlesSplasher : MonoBehaviour {
+
+    [SerializeField]
+    private List<ParticleSystem> m_colorsToChange;
+
+    public void SetColor(Color color)
+    {
+        foreach (ParticleSystem particleSys in m_colorsToChange)
+        {
+            ParticleSystem.MainModule main = particleSys.main;
+            main.startColor = color;        
+        }
+    }
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
