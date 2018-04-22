94ec81b84992060a5f4e811cd06a73901e468641
Vdela <volgix@yahoo.fr>
2018-04-22 10:32:35 +0200

    Hammer collider enable/disable, gunshot with gunsplash particles

---
+++ Unity/Assets/Scripts/Weapons/HammerAnimatorEvents.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerAnimatorEvents : MonoBehaviour {
+
+    [SerializeField]
+    private Collider hammerCollider;
+
+    void Start()
+    {
+        hammerCollider.enabled = false;
+    }
+
+    public void SetColliderActive()
+    {
+        hammerCollider.enabled = true;
+    }
+
+
+    public void SetColliderInactive()
+    {
+        hammerCollider.enabled = false;
+    }
+
+}
