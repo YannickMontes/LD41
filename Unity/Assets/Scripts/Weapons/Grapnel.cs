32539201f8c4af8b116b4958d15a936c788d249b
Vdela <volgix@yahoo.fr>
2018-04-22 17:38:57 +0200

    Grab WIP

---
+++ Unity/Assets/Scripts/Weapons/Grapnel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grapnel : Weapon {
+
+    public bool HasGrabbedSomething {
+        get; set;
+    }
+
+    public Grapnel(string name) : base(name)
+    {
+    }
+
+    public override void CastWeaponSkill()
+    {
+        if (m_animator)
+            m_animator.SetBool("TriggerGrab", true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Enemy enemyScript = other.gameObject.GetComponent<Enemy>();
+        if (enemyScript != null) {
+            PaintPlane(enemyScript, Vector3.zero, Vector3.zero);
+        }
+    }
+
+
+}
