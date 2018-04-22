333c0959409aebb155cbc74fca9209d361274114
Vdela <volgix@yahoo.fr>
2018-04-22 14:14:45 +0200

    ROSSMIT

---
+++ Unity/Assets/Scripts/Player/RossNeverTiltTheHead.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RossNeverTiltTheHead : MonoBehaviour {
+
+    private Quaternion rosstation;
+
+    void Awake()
+    {
+        rosstation = this.transform.rotation;
+    }
+
+	void Update () {
+        this.transform.rotation = rosstation;
+	}
+
+}
