5a99881a699d765d516fbb3d4cc6915dd69693cb
PapyKent <kentdu38@gmail.com>
2018-04-23 16:21:22 +0200

    debut de la refonte

---
+++ Unity/Assets/Scripts/ColorCube.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorCube : MonoBehaviour {
+
+    [SerializeField]
+    private List<Material> matList;
+
+    int currentColor = 0;
+    bool isChangingColor = false;
+
+    public void changeColor()
+    {
+        if (currentColor < matList.Count)
+        {
+            currentColor++;
+            isChangingColor = true;
+        }
+        else
+        {
+            currentColor = 0;
+            isChangingColor = false;
+        }
+            
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isChangingColor)
+        {
+            this.GetComponent<Renderer>().material = matList[currentColor];
+            isChangingColor = false;
+        }
+	}
+}
