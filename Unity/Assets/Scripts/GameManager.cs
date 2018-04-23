bc8ac534c50749df3304d5bff3e8c0f6ea91e93f
Yannick Montes <montes.yannick@gmail.com>
2018-04-23 16:10:48 +0200

    Game Manager for H button

--- Unity/Assets/Scripts/GameManager.cs
+++ Unity/Assets/Scripts/GameManager.cs
@@ -1,43 +1,36 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+	public static GameManager instance = null;
 
-    public static GameManager instance = null;
+	public GameObject targets;
+	public GameObject mobs;
 
-    public GameObject targets;
-    public GameObject mobs;
+	public GameObject commandPanel;
 
-    
-
-    void Awake()
-    {
-        //Check if instance already exists
-        if (instance == null)
-
-            //if not, set instance to this
-            instance = this;
-
-        //If instance already exists and it's not this:
-        else if (instance != this)
-
-            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
-            Destroy(gameObject);
-
-        //Sets this to not be destroyed when reloading scene
-        DontDestroyOnLoad(gameObject);
-
-    }
-
-
-    // Use this for initialization
-    void Start () {
-		
+	void Awake()
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);
+		DontDestroyOnLoad(gameObject);
 	}
-	
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.H))
+		{
+			commandPanel.SetActive(!commandPanel.active);
+		}
 	}
 }
