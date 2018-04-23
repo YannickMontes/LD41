a212befa322168f9bd700f821f6b24c302704541
Yannick Montes <montes.yannick@gmail.com>
2018-04-23 13:55:01 +0200

    Press H to hide/show help

--- Unity/Assets/Scripts/GameManager.cs
+++ Unity/Assets/Scripts/GameManager.cs
@@ -1,43 +1,45 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+	public static GameManager instance = null;
 
-    public static GameManager instance = null;
+	public GameObject targets;
+	public GameObject mobs;
 
-    public GameObject targets;
-    public GameObject mobs;
+	public GameObject commandPanel;
 
-    
+	private void Awake()
+	{
+		//Check if instance already exists
+		if (instance == null)
 
-    void Awake()
-    {
-        //Check if instance already exists
-        if (instance == null)
+			//if not, set instance to this
+			instance = this;
 
-            //if not, set instance to this
-            instance = this;
+		//If instance already exists and it's not this:
+		else if (instance != this)
 
-        //If instance already exists and it's not this:
-        else if (instance != this)
+			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+			Destroy(gameObject);
 
-            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
-            Destroy(gameObject);
-
-        //Sets this to not be destroyed when reloading scene
-        DontDestroyOnLoad(gameObject);
-
-    }
-
-
-    // Use this for initialization
-    void Start () {
-		
+		//Sets this to not be destroyed when reloading scene
+		DontDestroyOnLoad(gameObject);
 	}
-	
+
+	// Use this for initialization
+	private void Start()
+	{
+	}
+
 	// Update is called once per frame
-	void Update () {
-		
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.H))
+		{
+			commandPanel.SetActive(!commandPanel.activeSelf);
+		}
 	}
 }
