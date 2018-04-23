d25ce3afc2f1537468926c726ad490e6dffd2377
PapyKent <kentdu38@gmail.com>
2018-04-23 09:52:21 +0200

    menu v1

---
+++ Unity/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour {
+
+
+    private float coeff = 1.2f;
+
+	public void newGameButton(string newGameLevel){
+		SceneManager.LoadScene (newGameLevel);
+	}
+		
+	public void exitGameButton(){
+		Application.Quit ();
+	}
+
+}
