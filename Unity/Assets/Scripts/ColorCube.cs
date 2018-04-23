d19f37831dc628bb881a1548834840de97ae0519
YannickMontes <montes.yannick@gmail.com>
2018-04-23 21:36:16 +0200

    Color cube fix

--- Unity/Assets/Scripts/ColorCube.cs
+++ Unity/Assets/Scripts/ColorCube.cs
@@ -44,11 +44,11 @@ public class ColorCube : MonoBehaviour {
         }
     }
 
-   /* private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponentInParent<Weapon>() != null)
+        if (other.GetComponentInParent<Grapnel>() != null)
         {
             changeColor();
         }
-    }*/
+    }
 }
