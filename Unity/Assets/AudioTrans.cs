1d6ca4811a489c2d0489898e9fa94d5a2a465834
PapyKent <kentdu38@gmail.com>
2018-04-23 21:44:35 +0200

    gm et player prefabs + audio

---
+++ Unity/Assets/AudioTrans.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+
+
+public class AudioTrans : MonoBehaviour
+{
+    public AudioSource introductionSource;
+
+    public AudioSource audioSource;
+
+    void Start()
+    {
+
+        introductionSource.Play();
+
+        Invoke("playLoop", 30);
+    }
+
+    private void playLoop()
+    {
+        audioSource.Play();
+   
+    }
+}
